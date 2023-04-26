56de77f99ac1732ddaf808d4ada1f68595efcbc5
Shreya Sarker <12990165+shreyasarker@users.noreply.github.com>
2023-04-27 01:01:00 +0600

    Created SecondMaximumValueInArray.java

---
+++ SecondMaximumValueInArray.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+/***
+*
+* Find Second Maximum Value in an Array
+* 
+**/
+public class Main 
+{
+    public static int findSecondMaximum(int[] arr) 
+    {
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                secondMax = max;
+                max = arr[i];
+            }
+            else if (arr[i] > secondMax && arr[i] != max) {
+                secondMax = arr[i];
+            }
+        }
+       
+        return secondMax;
+    }
+    
+    public static void main(String[] args) throws Exception 
+    {
+        int[] arr = {13, 34, 2, 34, 33, 1, 5};
+        int maxValue = findSecondMaximum(arr);
+        System.out.println(maxValue);
+    }
+}

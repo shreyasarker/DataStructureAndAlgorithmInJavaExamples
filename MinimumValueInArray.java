043f17c018996477e0aba6e6e447b3102f6d7c7f
Shreya Sarker <12990165+shreyasarker@users.noreply.github.com>
2023-04-26 22:57:39 +0600

    Created MinimumValueInArray.java

---
+++ MinimumValueInArray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/***
+*
+* Find Minimum Value in an Array
+* 
+**/
+public class Main 
+{
+    public static int findMinimum(int[] arr) 
+    {
+        int min = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+       
+        return min;
+    }
+    
+    public static void main(String[] args) throws Exception 
+    {
+        int[] arr = {6, 11, 3, -7, 1, 4, 5};
+        int minValue = findMinimum(arr);
+        System.out.println(minValue);
+    }
+}

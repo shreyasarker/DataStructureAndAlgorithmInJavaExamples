42eeb06a11069f50c411e637657cbf2492d1e288
Shreya Sarker <12990165+shreyasarker@users.noreply.github.com>
2023-04-28 01:00:54 +0600

    Created MissingNumberInArray.java

---
+++ MissingNumberInArray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/***
+*
+* Missing Number in an Array
+* Approach: We will use Sum of Natural Numbers to solve this.
+* 1 + 2 + 3 + 4 + .... = n * (n + 1) / 2
+* 
+**/
+public class Main 
+{
+    public static int findMissingNumber(int[] arr) 
+    {
+        int n = arr.length + 1;
+        int sum = n * (n + 1) / 2;
+        for (int i = 0; i < (arr.length); i++) {
+            sum-=arr[i];
+        }
+        
+        return sum;
+    }
+    
+    public static void main(String[] args) throws Exception 
+    {
+        int[] arr = {2, 3, 4, 1, 5, 7};
+        int missingNumber = findMissingNumber(arr);
+        System.out.println(missingNumber);
+    }
+}
